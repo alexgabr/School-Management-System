f15348331115207f6fbca29ccc02cf9211af145d
alexgabr <117567665+alexgabr@users.noreply.github.com>
2023-06-20 21:58:49 +0300

    Files created; homepage ready

---
+++ src/controllers/pages/homepages/StaffHomepageController.java
@@ -0,0 +1,37 @@
+//
+//Copyright Alexandru Vrincianu
+//2023
+//
+package controllers.pages.homepages;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class StaffHomepageController implements Initializable {
+
+    @FXML
+    private Button bt_dashboard;
+
+    @FXML
+    private Button bt_salaries_menu;
+
+    @FXML
+    private Button bt_students_menu;
+
+    @FXML
+    private Button bt_teachers_menu;
+
+    @FXML
+    private Label lb_welcome;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
---
+++ src/controllers/pages/teachersPages/StaffTeachersController.java
@@ -0,0 +1,24 @@
+//
+//Copyright Alexandru Vrincianu
+//2023
+//
+package controllers.pages.teachersPages;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class StaffTeachersController implements Initializable{
+
+    @FXML
+    private Button bt_back;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+}
