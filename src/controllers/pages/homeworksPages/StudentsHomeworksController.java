f33e8bc0f36c3bcba1648ec0514f49867be5d434
alexgabr <117567665+alexgabr@users.noreply.github.com>
2023-06-21 11:15:21 +0300

    Students files created

---
+++ src/controllers/pages/homeworksPages/StudentsHomeworksController.java
@@ -0,0 +1,15 @@
+//
+//Copyright Alexandru Vrincianu
+//2023
+//
+package controllers.pages.homeworksPages;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StudentsHomeworksController {
+
+    @FXML
+    private Button bt_back;
+
+}
